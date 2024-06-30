d6c9d287f868a9c9dff3c28971f4aa953d0df0e1
Justin Dudley <justindudley99@gmail.com>
2024-06-30 16:32:13 -0400

    create tables folder

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ print("\nhello world yo\n")
 import pandas as pd 
 
 
-df_basics = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/basics.csv') 
+df_basics = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/basics.csv') 
 print("df_basics:\n")
 print(df_basics, "\n")
 
-df_transformations = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/transformations.csv') 
+df_transformations = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/transformations.csv') 
 print("df_transformations:\n")
 print(df_transformations, "\n")
