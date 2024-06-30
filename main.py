8cd14f0acc56e95f9e4a0d3d75ee355ea2401b4b
Justin Dudley <justindudley99@gmail.com>
2024-06-30 02:34:48 -0400

    second commit

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ print("hello world yo")
 
 import pandas as pd 
   
-df_1 = pd.read_excel('/Users/justindudley/MyDocuments/Cube/POCHMANN_FEVER_MAC/PYTHON_Repos/alg_prism_the_whole_enchilada/table_one.xlsx') 
+df_1 = pd.read_excel('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/one_table.xlsx') 
 print("df_1:")
 print(df_1)
 
-df_2 = pd.read_excel('/Users/justindudley/MyDocuments/Cube/POCHMANN_FEVER_MAC/PYTHON_Repos/alg_prism_the_whole_enchilada/table_two.xlsx') 
+df_2 = pd.read_excel('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/one_table.xlsx', sheet_name='Sheet1') 
 print("df_2:")
 print(df_2)
 
 # /Users/justindudley/MyDocuments/Cube/POCHMANN FEVER MAC/PYTHON Repos/alg_prism_the_whole_enchilada
+
+#excel_data_df = pandas.read_excel('records.xlsx', sheet_name='Employees')
+
+#/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen
