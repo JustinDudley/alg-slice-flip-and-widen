12c9ff991d7539c0a9416936097178bed81cf2c4
Justin Dudley <justindudley99@gmail.com>
2024-06-30 00:43:23 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#python program
+
+print("hello world yo")
+
+import pandas as pd 
+  
+df_1 = pd.read_excel('/Users/justindudley/MyDocuments/Cube/POCHMANN_FEVER_MAC/PYTHON_Repos/alg_prism_the_whole_enchilada/table_one.xlsx') 
+print("df_1:")
+print(df_1)
+
+df_2 = pd.read_excel('/Users/justindudley/MyDocuments/Cube/POCHMANN_FEVER_MAC/PYTHON_Repos/alg_prism_the_whole_enchilada/table_two.xlsx') 
+print("df_2:")
+print(df_2)
+
+# /Users/justindudley/MyDocuments/Cube/POCHMANN FEVER MAC/PYTHON Repos/alg_prism_the_whole_enchilada
