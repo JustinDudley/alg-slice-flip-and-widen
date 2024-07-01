ba2dcfc3b67de41f5e228205142b79772fe0a7ac
Justin Dudley <justindudley99@gmail.com>
2024-07-01 00:13:39 -0400

    create separate file for variables

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 
-print("\nhello world yo\n")
-
 import pandas as pd 
+from variables import *  
 
 
-df_basics = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/basics.csv') 
-print("df_basics:\n")
-print(df_basics, "\n")
-
-df_transformations = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/transformations.csv') 
-print("df_transformations:\n")
-print(df_transformations, "\n")
+print("df_basics:\n\n", df_basics, "\n")
+print("df_transformations:\n\n", df_transformations, "\n")
---
+++ variables.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+
+df_basics = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/basics.csv') 
+
+df_transformations = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/transformations.csv') 
+
