cf9901fb9ac2ac2012964d5aedf021f911a99458
Justin Dudley <justindudley99@gmail.com>
2024-07-04 18:07:35 -0400

    all 7 dataframes imported and assigned

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ import pandas as pd
 from variables import *  
 
 
-print("df_basics:\n\n", df_basics, "\n")
-print("df_transformations:\n\n", df_transformations, "\n")
+print("\n\ndf_basics:\n\n", df_basics, "\n")
+print("df_Ripple_R:\n\n", df_Ripple_L, "\n")
+print("df_Ripple_L:\n\n", df_Ripple_R, "\n")
+
+print("df_core:\n\n", df_core, "\n")
+print("df_turns:\n\n", df_turns, "\n")
+print("df_rotations:\n\n", df_rotations, "\n")
+print("df_reflections:\n\n", df_reflections, "\n")
+
+
