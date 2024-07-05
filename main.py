fd8b8523ba29709b149375ab701fa9e8e05f3875
Justin Dudley <justindudley99@gmail.com>
2024-07-05 00:00:56 -0400

    eliminate space between slice comps, create alg_two_components list

--- main.py
+++ main.py
@@ -3,13 +3,37 @@ import pandas as pd
 from variables import *  
 
 
-print("\n\ndf_basics:\n\n", df_basics, "\n")
-print("df_Ripple_R:\n\n", df_Ripple_L, "\n")
-print("df_Ripple_L:\n\n", df_Ripple_R, "\n")
+# print("\n\ndf_basics:\n\n", df_basics, "\n")
+# print("df_Ripple_R:\n\n", df_Ripple_L, "\n")
+# print("df_Ripple_L:\n\n", df_Ripple_R, "\n")
 
-print("df_core:\n\n", df_core, "\n")
-print("df_turns:\n\n", df_turns, "\n")
-print("df_rotations:\n\n", df_rotations, "\n")
-print("df_reflections:\n\n", df_reflections, "\n")
+# print("df_core:\n\n", df_core, "\n")
+# print("df_turns:\n\n", df_turns, "\n")
+# print("df_rotations:\n\n", df_rotations, "\n")
+# print("df_reflections:\n\n", df_reflections, "\n")
+
+# Must choose a group before running program. (1-6)
+group = 1
+
+alg = "U R2 U D' F B L' B2 R U D' B R L' U2 B2"
+
+
+
+
+
+# R L' -->  RL',  D U' --> U'D, etc.   This block replaces every instance of each substring in dict 
+for key, value in slice_comp_dict.items():
+	alg = alg.replace(key, value)
+print("alg after replacements:\n", alg)
+
+alg_list = alg.split()
+print("\nalg_list is:", alg_list)
+
+coro_scheme = group_dict[group]
+coro_scheme_list = coro_scheme.split()
+print(coro_scheme_list)
+
+alg_two_components = [alg_list, coro_scheme_list]
+print(alg_two_components)
 
 
--- variables.py
+++ variables.py
@@ -8,3 +8,38 @@ df_core = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/al
 df_turns = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/turns.csv') 
 df_rotations = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/rotations_24.csv') 
 df_reflections = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/reflections_24.csv') 
+
+slice_comp_dict = {
+    "R L\'": "RL\'", 
+    "L\' R": "RL\'", 
+    "R\' L": "R'L", 
+    "L R\'": "R\'L",
+    "R2 L2": "R2L2", 
+    "L2 R2": "R2L2",
+    "U D\'": "UD\'", 
+    "D\' U": "UD\'", 
+    "U\' D": "U'D", 
+    "D U\'": "U\'D",
+    "U2 D2": "U2D2", 
+    "D2 U2": "U2D2",
+    "F B\'": "FB\'", 
+    "B\' F": "FB\'", 
+    "F\' B": "F'B", 
+    "B F\'": "F\'B",
+    "F2 B2": "F2B2",
+    "B2 F2": "F2B2"
+    }
+
+group_dict = {
+    1: "X2 Y",
+    2: "X2 Y'",
+    3: "Y2 Z",
+    4: "Y2 Z'",
+    5: "Z2 X",
+    6: "Z2 X'"
+    }
+
+
+
+
+
