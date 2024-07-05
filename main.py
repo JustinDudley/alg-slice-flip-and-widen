ee261d06c9f6484acadc3947c22249179fbed5c0
Justin Dudley <justindudley99@gmail.com>
2024-07-05 00:41:00 -0400

    add Slice Comps dataframe

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from variables import *
 
 
 # print("\n\ndf_basics:\n\n", df_basics, "\n")
+# print("\n\ndf_slice_comps:\n\n", df_slice_comps, "\n")
 # print("df_Ripple_R:\n\n", df_Ripple_L, "\n")
 # print("df_Ripple_L:\n\n", df_Ripple_R, "\n")
 
@@ -12,12 +13,10 @@ from variables import *
 # print("df_rotations:\n\n", df_rotations, "\n")
 # print("df_reflections:\n\n", df_reflections, "\n")
 
+
 # Must choose a group before running program. (1-6)
 group = 1
-
 alg = "U R2 U D' F B L' B2 R U D' B R L' U2 B2"
-
-
 
 
 
@@ -33,7 +32,12 @@ coro_scheme = group_dict[group]
 coro_scheme_list = coro_scheme.split()
 print(coro_scheme_list)
 
+# alg_two_components:  [['U', 'R2', "UD'", 'F', 'B', "L'", 'B2', 'R', "UD'", 'B', "RL'", 'U2', 'B2'], ['X2', 'Y']]
 alg_two_components = [alg_list, coro_scheme_list]
-print(alg_two_components)
+
+for turn in alg_two_components[0]:
+      print(turn)
+      # now replace slice comps with slices, and alter the remainder of the alg
+      # will I be able to alter alg_two_components[0] directly?  It's a list within a list, which is tricky
 
 
--- variables.py
+++ variables.py
@@ -1,13 +1,15 @@
 import pandas as pd 
 
-df_basics = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/basics.csv') 
+df_basics = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/basics.csv')
+df_slice_comps = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/slice_comps.csv')
 df_Ripple_L = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/WCR_Ripples_Left.csv') 
 df_Ripple_R = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/WCR_Ripples_Right.csv') 
 
 df_core = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/core.csv') 
 df_turns = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/turns.csv') 
 df_rotations = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/rotations_24.csv') 
-df_reflections = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/reflections_24.csv') 
+df_reflections = pd.read_csv('/Users/justindudley/dev/cube/Alg_Slice_And_Widen_daddy/alg-slice-and-widen/tables/reflections_24.csv')
+
 
 slice_comp_dict = {
     "R L\'": "RL\'", 
