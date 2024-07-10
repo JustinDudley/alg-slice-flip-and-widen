ec72fb55d5565d60bc43214996c459fc0fb13d19
Justin Dudley <justindudley99@gmail.com>
2024-07-10 14:41:53 -0400

    all 3 DNAs appear to be working

--- Mod4_Comp_Xpansion.py
+++ Mod4_Comp_Xpansion.py
@@ -2,11 +2,15 @@ from dataframes import df_comp_rotation_nums
 
 
 
-def Mod4_Comp_Xpansion(alg_turns_shifting, trailing_YorZ_Xs, sum_successes):
+def Mod4_Comp_Xpansion(alg_turns_shifting, trailing_YorZ_Xs, DNA_3_marker_cums):
 
     print("inside Mod4_Comp_Xpansion, alg_turns_shifting is: ", alg_turns_shifting)
     print("inside Mod4_Comp_Xpansion, trailing_YorZ_Xs is: ", trailing_YorZ_Xs)
-    print("inside Mod4_Comp_Xpansion, sum_successes is: ", sum_successes)
+    print("inside Mod4_Comp_Xpansion, DNA_3_marker_cums is: ", DNA_3_marker_cums)
+
+    for DNA_3_marker_cum in DNA_3_marker_cums:
+        print(alg_turns_shifting)
+        print(DNA_3_marker_cum, "\n")
 
 
 
--- generate_algs.py
+++ generate_algs.py
@@ -3,7 +3,7 @@ import copy
 
 from Mod4_Comp_Xpansion import Mod4_Comp_Xpansion
 from dataframes import df_comp_rotation_nums
-from get_Sum_Successes import get_Sum_Successes_using_itertools
+from get_DNA_3_marker_cums import get_DNA_3_marker_cums_using_itertools
 from helper_methods import get_comp_turn_codes
 
 
@@ -17,14 +17,14 @@ def generate_algs(alg_turns:list[str], trailing_YorZ_Xs_dual):
         alg_turns_plus_YorZ_static.append(trailing_YorZ_Xs[0])  # YorZ appended
         alg_turns_plus_YorZ_shifting = copy.deepcopy(alg_turns_plus_YorZ_static)
         comp_turn_codes = copy.deepcopy(list(map(get_comp_turn_codes, alg_turns_plus_YorZ_static)))
-        sum_successes = get_Sum_Successes_using_itertools(comp_turn_codes, trailing_YorZ_Xs)
+        DNA_3_marker_cums = get_DNA_3_marker_cums_using_itertools(comp_turn_codes, trailing_YorZ_Xs)
         print("alg_turns_plus_YorZ_static is: ", alg_turns_plus_YorZ_static, "\n")
 
 
         for index, alg_turn in reversed(list(enumerate(alg_turns_plus_YorZ_static))):
             print("index and single alg_turn, in reverse order, are: ", index, ",  ", alg_turn) 
             
-            one_round_of_final_algs = Mod4_Comp_Xpansion(alg_turns_plus_YorZ_shifting, trailing_YorZ_Xs, sum_successes)
+            one_round_of_final_algs = Mod4_Comp_Xpansion(alg_turns_plus_YorZ_shifting, trailing_YorZ_Xs, DNA_3_marker_cums)
             final_algs_from_single_SticSolv.append(one_round_of_final_algs)   
             print("Here is final_alg_pipe_string so far: ", final_algs_from_single_SticSolv, "\n")
             #Call the Ripple_L method, so YorZ moves one to the left before this loop repeats
