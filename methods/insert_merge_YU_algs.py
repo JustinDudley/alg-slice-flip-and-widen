581fb69ec2aac4095100a8836f24bb46e3e04dba
Justin Dudley <justindudley99@gmail.com>
2025-03-15 00:21:04 -0400

    fix merge_YU. It wasn't catching all the u,d,f,b because I was adding a trailing space to the alg on the wrong line of code

--- methods/insert_merge_YU_algs.py
+++ methods/insert_merge_YU_algs.py
@@ -5,9 +5,10 @@ def insert_merge_YU_algs(one_ripple_round_of__final_algs):
     bfud_inserted__one_ripple_round_of__final_algs = []
     for alg in one_ripple_round_of__final_algs:
         bfud_inserted__one_ripple_round_of__final_algs.append(alg)
-        for key, value in MERGE_YU_DICT.items():  
+
+        alg = alg + " "     # so that the search ("if key in alg") works even for turns that aren't counterclockwise or double
+        for key, value in MERGE_YU_DICT.items():
             if key in alg:                 # because don't want to append again if no change
-                alg = alg + " "     # so that the search works even for turns that aren't counterclockwise or double
                 alg = alg.replace(key, value)
                 alg = alg.strip()
                 bfud_inserted__one_ripple_round_of__final_algs.append(alg)
