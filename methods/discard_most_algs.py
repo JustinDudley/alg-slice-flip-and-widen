d8b01ca63950c2e7710db8fb431a4eb1fb7db984
Justin Dudley <justindudley99@gmail.com>
2025-05-26 11:41:18 -0400

    discard_most_algs is functional, whole app is functional and hopefully done

--- methods/discard_most_algs.py
+++ methods/discard_most_algs.py
@@ -1,9 +1,25 @@
 
-
 def discard_most_algs(all_algs):
-    subset_of_algs = []
+    
+    good_subset_of_algs = []
     for alg in all_algs:
-        subset_of_algs.append(alg)
+
+        # include algs with u,d,f,b
+        for turn_letter in ["u", "d", "f", "b"]:
+            if alg.find(turn_letter) >=0:
+                good_subset_of_algs.append(alg)
 
 
-    return subset_of_algs
+        # include algs whose SECOND turn (after the leading X) is a Y or Z WCR  (includes Y', Z2, etc.)
+        first_whitespace_index = alg.find(" ")
+        if alg[first_whitespace_index + 1] in ["Y", "Z"]:
+            good_subset_of_algs.append(alg)
+        
+
+         # include algs whose FINAL turn is a Y or Z WCR  (includes Y', Z2, etc.)
+        last_whitespace_index = alg.rfind(" ")
+        if alg[last_whitespace_index + 1] in ["Y", "Z"]:
+            good_subset_of_algs.append(alg)       
+
+
+    return good_subset_of_algs
